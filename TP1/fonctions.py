5e2dfda97230cc6a6eab221e8b5ca335190753f3
ToToreFR <thomasdehais06@gmail.com>
2025-09-04 16:34:47 +0200

    ajout 6

--- TP1/fonctions.py
+++ TP1/fonctions.py
@@ -1,10 +1,3 @@
-
-def test_exc_1():
-   with pytest.raises(Exception):
-       puissance(0,-1)
-       puissance(0,2)
-
-
 def puissance(a,b):
   if not type(a) is int:
      raise TypeError("Only integers are allowed")
@@ -18,3 +11,8 @@ def puissance(a,b):
        for _ in range(b):
         result *= a
        return result
+
+def test_exc_1():
+   with pytest.raises(Exception):
+       puissance(0,-1)
+       puissance(0,2)
