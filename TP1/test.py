02ca00cbaea5e5de69af918c1f2bf0727d738cd5
ToToreFR <thomasdehais06@gmail.com>
2025-09-04 15:37:21 +0200

    ajout md

--- TP1/test.py
+++ TP1/test.py
@@ -11,4 +11,5 @@ def test_2():
      assert f.puissance(-1,-1) == -1
      assert f.puissance(-1,-2) == 1
      assert f.puissance(-2,-1) == -0.5
-     assert f.puissance(0,-1) == 0
+     assert f.puissance(2, 2) == 4
+     assert f.puissance(1,1) == 1
