d4be15896e13ce79475894113d89efe948f7c99b
ToToreFR <thomasdehais06@gmail.com>
2025-09-04 16:52:40 +0200

    ajout 6

--- TP1/fonctions.py
+++ TP1/fonctions.py
@@ -1,7 +1,7 @@
 def puissance(a,b):
   if not type(a) is int:
      raise TypeError("Only integers are allowed")
-  if a== 0 and b < 0:
+  if a == 0 and b < 0:
      raise Exception("mettre supérieur à 0")
   else:
      result = 1
