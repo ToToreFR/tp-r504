a82f57f2c57b8030eaa6dfbdaf793232364714df
ToToreFR <thomasdehais06@gmail.com>
2025-09-04 15:52:27 +0200

    ajout md

--- TP1/fonctions.py
+++ TP1/fonctions.py
@@ -1,8 +1,8 @@
 def puissance(a,b):
-	if not type(a) is int:
-		raise TypeError("Only integers are allowed")
-	else:
-		result = 1
-		for _ in range(b):
-			result *= a
-		return result if b < 0 else 1/result
+  if not type(a) is int:
+     raise TypeError("Only integers are allowed")
+  else:
+    result = 1
+    for _ in range(b):
+       result *= a
+    return result if b < 0 else 1/results
