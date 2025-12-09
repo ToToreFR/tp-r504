04d681ac8057c8fb947a9c55525b7e398727efba
user <user@vbox.univ-rouen.fr>
2025-12-09 09:56:20 -0500

    ajout

--- TP_LB/start.sh
+++ TP_LB/start.sh
@@ -1,9 +1,9 @@
 docker network create --driver bridge tplb
-docker build -t im-nginx-lb .
+docker build -t im-nginx-lb ./tp-A
 mkdir -p shared1 shared2
 echo "<h1>Hello 1</h1>" > shared1/index.html
 echo "<h1>Hello 2</h1>" > shared2/index.html
-docker run -d --name nginx1 --rm -p 81:80 -v "$(pwd)/shared1:/usr/share/nginx/html" nginx
-docker run -d --name nginx2 --rm -p 82:80 -v "$(pwd)/shared2:/usr/share/nginx/html" nginx
-docker run -d --name nginx-lb --rm -p 83:80 --network tplb im-nginx-lb
+docker run -d --name nginx1 --rm --network tplb -p 81:80 -v "$(pwd)/shared1:/usr/share/nginx/html" nginx
+docker run -d --name nginx2 --rm --network tplb -p 82:80 -v "$(pwd)/shared2:/usr/share/nginx/html" nginx
+docker run -d --name nginx-lb --rm --network tplb -p 83:80 im-nginx-lb
 
---
+++ TP_LB/stop.sh
@@ -0,0 +1,3 @@
+docker stop nginx-lb nginx1 nginx2 2>/dev/null
+docker rm nginx-lb nginx1 nginx2 2>/dev/null
+docker rm network tplb 2>/dev/null
